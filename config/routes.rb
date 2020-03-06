93f771a4603147963eb3a31dbd38d33fce678f33
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2020-03-05 21:26:58 -0600

    [New] Route all html requests back to static controller, which will serve the React app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root to: 'static#index'
 end
