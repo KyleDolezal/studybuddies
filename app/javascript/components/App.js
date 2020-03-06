4e4a28fe471d14a370408ecf8c2ed9dde6de0b8c
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2020-03-05 22:07:10 -0600

    [Chore] Set up React Javascript

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div></div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
