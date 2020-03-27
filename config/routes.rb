4b316db7a0f5984d74674cdcbe77880edc558cf6
Kyle Dolezal <kyle.dolezal@thoughtworks.com>
2020-03-26 19:52:58 -0500

    [New] Add route for user interests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
+  post '/user_interests', to: 'user_interests#create', constraints: { format: 'json' }
+
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
