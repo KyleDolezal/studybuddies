4b2cd758d2e1bdb8f3190f5e995b463b4527e768
KyleDolezal <kyle.d.dolezal@gmail.com>
2020-03-26 19:51:50 -0500

    [Chore] Clean up handling of http headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protected
+  def set_headers
+    response.headers['access-token'] = request.headers['access-token']
+    response.headers['client'] = request.headers['client']
+    response.headers['expiry'] = request.headers['expiry']
+    response.headers['uid'] = request.headers['uid']
+  end
 end
