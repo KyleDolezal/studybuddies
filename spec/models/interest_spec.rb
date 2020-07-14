98f2db4f1e6ae22c3c459c462579d531fe6f38fb
KyleDolezal <kyle.d.dolezal@gmail.com>
2020-07-13 22:27:40 -0500

    [Fix] Validate length of interest title

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Interest, :type => :model do
     it { should have_and_belong_to_many(:users) }
     it { should have_many(:user_interests) }
   end
+
+  context "validations" do
+     it { should validate_length_of(:title).is_at_least(1) }
+  end
 end
