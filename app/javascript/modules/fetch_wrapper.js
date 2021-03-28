1733a5cbb64a9377625616550e5d18f1444e5511
KyleDolezal <kyle.d.dolezal@gmail.com>
2021-03-27 20:07:54 -0500

    [Fix] Correct typo in fetch wrapper

--- app/javascript/modules/fetch_wrapper.js
+++ app/javascript/modules/fetch_wrapper.js
@@ -16,7 +16,7 @@ const fetch_with_auth_headers = (path, fetch_params, auth_token, setAuthToken) =
     setAuthToken({
       'access-token': response.headers.get('access-token'),
       'client': response.headers.get('client'),
-      'expiry': response.headers.get('expirty'),
+      'expiry': response.headers.get('expiry'),
       'uid': response.headers.get('uid')
     })
     return response
